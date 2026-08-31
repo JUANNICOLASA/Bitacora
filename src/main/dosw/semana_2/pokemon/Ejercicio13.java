b7ad9883855cb6ab46ffb0be4d305df4c7688ffa
JUANNICOLASA <juan.amunoz@mail.escuelaing.edu.co>
2026-08-31 14:34:19 -0500

    feat: solucion reto pokemon 14

--- src/main/dosw/semana_2/pokemon/Ejercicio13.java
+++ src/main/dosw/semana_2/pokemon/Ejercicio13.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 public class Ejercicio13 {
     public static Map<String, List<Pokemon>> agruparPorTipo(List<Pokemon> pokemons) {
-        return pokemons.stream().distinct().collect(Collectors.groupingBy(Pokemon::getTipo)); // *Reto Legendario
+        return pokemons.stream().distinct().collect(Collectors.groupingBy(Pokemon::getTipo));
     }
     public static void main(String[] args) {
         agruparPorTipo(MockData.getPokemons()).forEach((t, l) -> System.out.println(t + ": " + l.stream().map(Pokemon::getNombre).toList()));
